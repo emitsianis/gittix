ff808837f03a3549a9819cec9b58acaa8a28f6c9
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-08-15 14:22:02 +0300

    Add: Request hook, initial props logic, navbar and functionality

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import useRequest from '../../hooks/use-request';
 import Router from 'next/router';
 
-const Signup = () => {
+const Signin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { errors, doRequest } = useRequest({
-    url: '/api/users/signup',
+    url: '/api/users/signin',
     method: 'post',
     body: { email, password },
     onSuccess: () => Router.push('/'),
@@ -19,7 +19,7 @@ const Signup = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
+      <h1>Sign In</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -38,9 +38,9 @@ const Signup = () => {
         />
       </div>
       {errors}
-      <button className="btn btn-primary">Sign Up</button>
+      <button className="btn btn-primary">Sign In</button>
     </form>
   );
 };
 
-export default Signup;
+export default Signin;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,16 @@
-const Index = () => {
-  return (
-    <h1>
-      Landing Page
-    </h1>
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
   );
 };
 
-export default Index;
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
