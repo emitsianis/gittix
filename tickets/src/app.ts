82735027477bc7c1e72ccd3d858058001785b672
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-08-17 20:10:45 +0300

    Add: Testing setup for tickets, create new ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,8 @@
 import 'express-async-errors';
 import express, { json } from 'express';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@emitsianis-gittix/common';
+import { currentUser, errorHandler, NotFoundError } from '@emitsianis-gittix/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -10,6 +11,10 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test',
 }));
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { requireAuth } from '@emitsianis-gittix/common';
+
+const router = express.Router();
+
+router.post(
+  '/api/tickets',
+  requireAuth,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+export { router as createTicketRouter };
