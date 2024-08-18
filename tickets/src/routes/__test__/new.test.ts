c8c178088188eb6a023e43a45f2cdb7fa46888ab
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-08-18 12:16:24 +0300

    Add: CRUD tickets

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import { app } from '../../app';
 import request from 'supertest';
+import { Ticket } from '../../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -58,4 +59,23 @@ it('returns an error if an invalid price provided', async () => {
 });
 
 it('creates a ticket with valid input', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = 'Title';
+  const price = 20;
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(price);
+  expect(tickets[0].title).toEqual(title);
 });
