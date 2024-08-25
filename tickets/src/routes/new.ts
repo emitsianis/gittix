15940fadd94e6001afb7cb63e7dd56f6167fcfcb
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-08-25 12:28:26 +0300

    Add: Make tests work with nats

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -31,7 +31,7 @@ router.post(
 
     await ticket.save();
 
-    await new TicketCreatedPublisher(natsWrapper.client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
