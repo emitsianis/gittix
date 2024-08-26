2e8936e8583c1ce59ad55b4777ea6cbac5aab595
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-08-26 19:35:53 +0300

    Fix: Typo

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -20,7 +20,7 @@ app.use(currentUser);
 app.use(indexOrderRouter);
 app.use(newOrderRouter);
 app.use(showOrderRouter);
-app.use(deleteOrderRouterc);
+app.use(deleteOrderRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
