348044331fd61faa3c1e65405f0a7ba424700c6b
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-08-27 19:16:04 +0300

    Fix: Common lib version for orders

--- orders/routes/new.ts
+++ orders/routes/new.ts
@@ -1,11 +1,44 @@
 import { Request, Response, Router } from 'express';
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@emitsianis-gittix/common';
+import { body } from 'express-validator';
+import * as mongoose from 'mongoose';
+import { Ticket } from '../src/models/ticket';
+import { Order } from '../src/models/order';
 
 const router = Router();
 
 router.post(
   '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
   },
 );
 
